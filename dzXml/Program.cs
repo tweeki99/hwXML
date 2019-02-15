8069f1ade635bce2c6ad5534938d4ca012a4f436
tweeki99 <danil.shipilov.99@gmail.com>
2019-02-15 12:29:45 +0600

    hwXML

---
+++ dzXml/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dzXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car {
+                Model = "Mercedes Benz",
+                Colour = "Black",
+                Toning = true,
+                Wheels = 4
+            };
+        }
+    }
+}
